74dde15b6243b0b456b6b4c3c1d932ec12bb6193
vipulbadwaik <vipul.badwaik@gmail.com>
2020-10-31 22:36:34 +0530

    Day1

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.txt}>BMI </Text>
+      <View style={styles.in}>
+        <TextInput style={styles.weight} placeholder="Weight" />
+        <TextInput style={styles.height} placeholder="Height" />
+        <TouchableOpacity
+          style={{ height: "100%", width: "100%", alignItems: "center" }}
+        >
+          <Text style={styles.cal}>Calculate</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -14,8 +28,47 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "orange",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: StatusBar.currentHeight,
+  },
+  txt: {
+    flex: 1,
+    textAlign: "center",
+    textAlignVertical: "center",
+    fontSize: 60,
+  },
+  in: {
+    flex: 1.5,
+    width: "100%",
+    alignItems: "center",
+  },
+  weight: {
+    height: "12%",
+    width: "90%",
+    backgroundColor: "white",
+    margin: 10,
+    textAlign: "center",
+    borderRadius: 30,
+  },
+  height: {
+    height: "12%",
+    width: "90%",
+    backgroundColor: "white",
+    margin: 10,
+    textAlign: "center",
+    borderRadius: 30,
+  },
+  cal: {
+    height: "12%",
+    width: "50%",
+    backgroundColor: "#e81e1e",
+    marginTop: 50,
+    textAlign: "center",
+    textAlignVertical: "center",
+    borderRadius: 30,
+    fontSize: 25,
+    color: "white",
   },
 });
